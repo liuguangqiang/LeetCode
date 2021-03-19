3cff298892a7d314bde6a958821bee557d639312
Eric <guangqiang.dev@gmail.com>
2021-03-19 16:29:07 +0800

    ReverseLinkedList

---
+++ src/com/liuguangqiang/leetcode/ReverseLinkedList.java
@@ -0,0 +1,45 @@
+package com.liuguangqiang.leetcode;
+
+import com.liuguangqiang.structure.linkedlist.LinkedList;
+import com.liuguangqiang.structure.linkedlist.ListNode;
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ * <p>
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * <p>
+ * Created by Eric at 2021/3/19 4:16 下午
+ */
+public class ReverseLinkedList {
+
+    /**
+     * 使用递归
+     * <p>
+     * 假设整个链表只有两个节点head和next， 那么要反转链表， 只需要把链表中的两个节点换一个位置就可以了;
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        ListNode newHead = reverseList(next);
+        next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        LinkedList linkedList = new LinkedList(head);
+        linkedList.add(new ListNode(5));
+        linkedList.add(new ListNode(4));
+        linkedList.add(new ListNode(3));
+        linkedList.add(new ListNode(2));
+        linkedList.add(new ListNode(10));
+        linkedList.head = reverseList(head);
+        linkedList.print();
+    }
+}
