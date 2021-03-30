080b29986650302402e2aad6bd778fe9d5a1419e
Eric <guangqiang.dev@gmail.com>
2021-03-30 10:06:20 +0800

    add structure TreeNode

---
+++ src/com/liuguangqiang/structure/tree/TreeNode.java
@@ -0,0 +1,17 @@
+package com.liuguangqiang.structure.tree;
+
+/**
+ * Created by Eric at 2021/3/30 10:05 上午
+ */
+public class TreeNode {
+
+    public int val;
+
+    public TreeNode left;
+
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
